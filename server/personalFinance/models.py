8b19ee2c56c928eb17ea70eac2809f05fd245226
unknown <miguel.g.c.505@gmail.com>
2021-01-21 18:02:01 +0100

    Simplify UI files and tree

--- server/personalFinance/models.py
+++ server/personalFinance/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    pass
+
+class Stock(models.Model):
+    pass
+
+class Crypto(models.Model):
+    pass
+
+class Income(models.Model):
+    pass
+
+class Outcome(models.Model):
+    pass
+
+class Buy(models.Model):
+    pass
+
+class Sell(models.Model):
+    pass
