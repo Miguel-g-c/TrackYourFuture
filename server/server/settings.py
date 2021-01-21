c0583e2994358d520f3d659f9f8adbd6cbda09a9
unknown <miguel.g.c.505@gmail.com>
2021-01-21 10:08:18 +0100

    Revert to febd980ce66a9ed72c8d5f2b242e9e9d4d8bd09f

--- server/server/settings.py
+++ server/server/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from secret_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,6 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ')vn8kf)xb#xp4h@*&=wxqq2ztr#8jumdxe3puj7&^%@&-z_2rq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
