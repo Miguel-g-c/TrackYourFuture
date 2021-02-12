7d962265c2ccc01691bb897139c5abd7b02936a9
Miguel-g-c <miguel.g.c.505@gmail.com>
2021-02-12 16:36:01 +0100

    Adapt media queries in homepage

--- tyf-ui/src/App.js
+++ tyf-ui/src/App.js
@@ -5,7 +5,7 @@ import {
   Route,
   Redirect,
 } from 'react-router-dom'
-import { ChakraProvider, Box, Grid, theme } from '@chakra-ui/react'
+import { ChakraProvider, Box, theme } from '@chakra-ui/react'
 import { Navbar } from './components/Navbar'
 import { Footer } from './components/Footer'
 import Home from './pages/Home'
@@ -50,28 +50,27 @@ function App() {
     <Router>
       <ChakraProvider theme={theme}>
         <Navbar user={user} handleLogout={handleLogout} />
-        <Box className="main" textAlign="center" fontSize="xl">
-          <Grid minH="90vh">
-            <Switch>
-              <Route path="/login">
-                {user ? (
-                  <Redirect to="/" />
-                ) : (
-                  <Login userHandler={userHandler} />
-                )}
-              </Route>
-              <Route path="/register">
-                {user ? (
-                  <Redirect to="/" />
-                ) : (
-                  <Register handleRegister={handleRegister} />
-                )}
-              </Route>
-              <Route path="/">
-                <Home user={user} />
-              </Route>
-            </Switch>
-          </Grid>
+        <Box
+          className="main"
+          textAlign="center"
+          fontSize="xl"
+          minH={{ base: '75vh', md: '85vh', lg: '90vh' }}
+        >
+          <Switch>
+            <Route path="/login">
+              {user ? <Redirect to="/" /> : <Login userHandler={userHandler} />}
+            </Route>
+            <Route path="/register">
+              {user ? (
+                <Redirect to="/" />
+              ) : (
+                <Register handleRegister={handleRegister} />
+              )}
+            </Route>
+            <Route path="/">
+              <Home user={user} />
+            </Route>
+          </Switch>
         </Box>
         <Footer />
       </ChakraProvider>
