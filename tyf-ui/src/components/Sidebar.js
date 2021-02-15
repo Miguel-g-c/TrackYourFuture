9983846977461acf290154a824e57f665642813c
Miguel-g-c <miguel.g.c.505@gmail.com>
2021-02-15 10:03:10 +0100

    Finish sidebar component

--- tyf-ui/src/components/Sidebar.js
+++ tyf-ui/src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import {
   Drawer,
@@ -12,6 +12,7 @@ import {
   Button,
   useColorModeValue,
 } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 import { MdDashboard, MdSettings } from 'react-icons/md'
 import { GiReceiveMoney, GiPayMoney } from 'react-icons/gi'
 import { AiOutlineStock } from 'react-icons/ai'
@@ -20,6 +21,23 @@ import darkLogo from './static/tyf_dark.png'
 
 export const Sidebar = props => {
   const logo = useColorModeValue(lightLogo, darkLogo)
+  const [active, setActive] = useState({
+    dashboard: false,
+    account: false,
+    incomes: false,
+    expenses: false,
+    assets: false,
+  })
+
+  function handleClick(property) {
+    for (const prop in active) {
+      if (property === prop) {
+        setActive(prevActive => ({ ...prevActive, [prop]: true }))
+      } else {
+        setActive(prevActive => ({ ...prevActive, [prop]: false }))
+      }
+    }
+  }
 
   return (
     <Drawer
@@ -39,51 +57,81 @@ export const Sidebar = props => {
           </DrawerHeader>
 
           <DrawerBody>
-            <Button
-              leftIcon={<MdDashboard />}
-              colorScheme="teal"
-              variant="outline"
-              isFullWidth
-              my={2}
-            >
-              Dashboard
-            </Button>
-            <Button
-              leftIcon={<MdSettings />}
-              colorScheme="teal"
-              variant="outline"
-              isFullWidth
-              my={2}
-            >
-              Account settings
-            </Button>
-            <Button
-              leftIcon={<GiReceiveMoney />}
-              colorScheme="teal"
-              variant="outline"
-              isFullWidth
-              my={2}
-            >
-              Incomes
-            </Button>
-            <Button
-              leftIcon={<GiPayMoney />}
-              colorScheme="teal"
-              variant="outline"
-              isFullWidth
-              my={2}
-            >
-              Expenses
-            </Button>
-            <Button
-              leftIcon={<AiOutlineStock />}
-              colorScheme="teal"
-              variant="outline"
-              isFullWidth
-              my={2}
-            >
-              Assets
-            </Button>
+            <Link to="/dashboard">
+              <Button
+                leftIcon={<MdDashboard />}
+                colorScheme="teal"
+                variant="ghost"
+                height="35px"
+                justifyContent="flex-start"
+                isFullWidth
+                isActive={active.dashboard}
+                onClick={() => handleClick('dashboard')}
+                my={2}
+              >
+                Dashboard
+              </Button>
+            </Link>
+            <Link to="/account">
+              <Button
+                leftIcon={<MdSettings />}
+                colorScheme="teal"
+                variant="ghost"
+                height="35px"
+                justifyContent="flex-start"
+                isFullWidth
+                isActive={active.account}
+                onClick={() => handleClick('account')}
+                my={2}
+              >
+                Account
+              </Button>
+            </Link>
+            <Link to="/incomes">
+              <Button
+                leftIcon={<GiReceiveMoney />}
+                colorScheme="teal"
+                variant="ghost"
+                height="35px"
+                justifyContent="flex-start"
+                isFullWidth
+                isActive={active.incomes}
+                onClick={() => handleClick('incomes')}
+                my={2}
+              >
+                Incomes
+              </Button>
+            </Link>
+            <Link to="/expenses">
+              <Button
+                leftIcon={<GiPayMoney />}
+                colorScheme="teal"
+                variant="ghost"
+                height="35px"
+                justifyContent="flex-start"
+                isFullWidth
+                isActive={active.expenses}
+                onClick={() => handleClick('expenses')}
+                my={2}
+              >
+                Expenses
+              </Button>
+            </Link>
+            <Link to="/assets">
+              <Button
+                leftIcon={<AiOutlineStock />}
+                colorScheme="teal"
+                variant="ghost"
+                height="35px"
+                justifyContent="flex-start"
+                isFullWidth
+                isActive={active.assets}
+                onClick={() => handleClick('assets')}
+                my={2}
+              >
+                Assets
+              </Button>
+            </Link>
           </DrawerBody>
         </DrawerContent>
       </DrawerOverlay>
